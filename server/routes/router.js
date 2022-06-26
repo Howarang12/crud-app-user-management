f1b682ffee4771e22c3f035c548d56b42b617e6c
Howard Wang <howard122696@gmail.com>
2022-06-25 20:58:50 -0700

    create post request

--- server/routes/router.js
+++ server/routes/router.js
@@ -3,18 +3,25 @@ const express = require('express')
 const route = express.Router()
 
 const services = require('../services/render')
+const controller = require('../controller/controller')
 
-// @description Root Router
+// @description Root Router page
 // @Method GET /
 route.get('/' , services.homeRoutes)
 
-// @description add users
+// @description add users page
 // @Method GET /add-user
 route.get('/add-user', services.add_user)
 
 
-// @description for update user
+// @description for update user page
 // @Method GET /update-user
 route.get('/update-user', services.update_user)
+
+// API
+route.post('/api/users', controller.create)
+route.get('/api/users', controller.find)
+route.put('/api/users/:id', controller.update)
+route.delete('/api/users/:id', controller.delete)
 
 module.exports = route
